3eda80a7be7d24cb61f2b52fbf09db2cd4f7edce
Bart <bart.tassignon@student.ehb.be>
2020-04-28 22:22:12 +0200

    Test van Bart

---
+++ Bart/src/LoginGui/TestBart.java
@@ -0,0 +1,15 @@
+/**
+ * @Author Bart Tassignon
+ */
+
+package LoginGui;
+
+public class TestBart {
+    public static void main(String[] args) {
+        System.out.println("Dit is een test van Bart!");
+    }
+
+
+}
+
+
