95ba7e8cca278ade4bba02566655ca37f76a9472
Bart <bart.tassignon@student.ehb.be>
2020-04-30 13:14:14 +0200

    Klasse Adres toegevoegd Adres toegevoegd aan klasse Lezer.java

--- Bart/src/entity/Lezer.java
+++ Bart/src/entity/Lezer.java
@@ -12,7 +12,8 @@ public class Lezer {
     private String voornaam;
     private String naam;
     private Date geboortedatum;
-    private String adres;
+    // toegevoegd door Bart na aanmaak klasse Adres
+    private Adres adres;
     private String email;
     private int telefoon;
     private String gebruikersnaam;
