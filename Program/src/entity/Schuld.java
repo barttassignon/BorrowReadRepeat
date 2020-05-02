af2d445540c275cfe149f5b036ea4aefde83b8b6
vosje16 <virginie.ortegat@student.ehb.be>
2020-05-02 22:59:37 +0200

    Enum gemaakt van Oorsprong, aangezien er maar 4 beperkte mogelijkheden zijn.

--- Program/src/entity/Schuld.java
+++ Program/src/entity/Schuld.java
@@ -3,18 +3,19 @@ package entity;
 import java.time.LocalDate;
 
 public class Schuld {
-    private String oorsprong;
+    public enum Oorsprong {OVERTIJD, VERLIES, BESCHADIGING, RESERVATIE}
+    private Oorsprong oorsprong;
     private double bedrag;
     private LocalDate datumAangemaakt;
     private LocalDate datumBetaald;
 
-public Schuld(LocalDate datumAangemaakt, String oorsprong, double bedrag){
+public Schuld(LocalDate datumAangemaakt, Oorsprong oorsprong, double bedrag){
     this.datumAangemaakt = datumAangemaakt;
     this.oorsprong = oorsprong;
     this.bedrag = bedrag;
 }
 
-public String getOorsprong() {
+public Oorsprong getOorsprong() {
     return oorsprong;
 }
 
