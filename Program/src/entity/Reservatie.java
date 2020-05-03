3225a1afab6cebd20291e758b29dd80310f001e5
ckaers <charlienkaers@gmail.com>
2020-05-03 12:54:18 +0200

    Klasse uitlening

--- Program/src/entity/Reservatie.java
+++ Program/src/entity/Reservatie.java
@@ -28,18 +28,22 @@ public class Reservatie extends Transactie{
                 +", reservatiedatum = " + reservatieDatum;
     }
 }
-public class HashSetBoek {
-    public static void main(String[] args) {
-        HashSet<Boek> set=new HashSet<>();
+
+// hashset geeft foutmelding bij ISBN-nr
+
+//public class HashSetBoek {
+//    public static void main(String[] args) {
+//        HashSet<Boek> set=new HashSet<>();
         //Boeken aanmaken
-        Boek b1=new Boek(101, 9789401611039,"Zon","Lucinda Riley","Xander Uitgevers B.V.", "Nederlands", 732, 13-2-2020, 24.99, "RIRO",0, 2);
-        Boek b2=new Boek(102,9789026343339, "In hechtenis", "Nicci French", "Ambo|Anthos", "Nederlands", 437, 7-4-2020, 22.99, "FRRO", 1, 5);
-        //Boeken aan HashSet toevoegen
-        set.add(b1);
-        set.add(b2);
+//        Boek b1=new Boek(101, 9789401611039L,"Zon","Lucinda Riley","Xander Uitgevers B.V.", "Nederlands", 732, 13-2-2020, 24.99, "RIRO",0, 2);
+//        Boek b2=new Boek(102, 9789026343339L, "In hechtenis", "Nicci French", "Ambo|Anthos", "Nederlands", 437, 7-4-2020, 22.99, "FRRO", 1, 5);
+//        //Boeken aan HashSet toevoegen
+//        set.add(b1);
+//        set.add(b2);
         //HashSet overnemen
-        for(Boek b:set){
-            System.out.println(b.getArtikelnummer()+" "+b.getISBN()+" "+b.getTitel()+" "+b.getAuteur()+" "+b.getUitgeverij()+""+b.getTaal()+" "+b.getPaginas()+" "+b.getAankoopdatum()+" "+b.getPrijs()+" "+b.getPlaatsInBib()+""+b.isGereserveerd()+" "+b.getAantalKeerUitgeleend());
-        }
-    }
-}
+//        for(Boek b:set){
+//            System.out.println(b.getArtikelnummer()+" "+b.getISBN()+" "+b.getTitel()+" "+b.getAuteur()+" "+b.getUitgeverij()+""+b.getTaal()+" "+b.getPaginas()+" "+b.getAankoopdatum()+" "+b.getPrijs()+" "+b.getPlaatsInBib()+""+b.isGereserveerd()+" "+b.getAantalKeerUitgeleend());
+//        }
+//    }
+    
+//}
