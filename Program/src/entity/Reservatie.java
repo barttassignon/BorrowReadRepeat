2476a74af44a5619293559863a1abf83d9a11834
ckaers <charlienkaers@gmail.com>
2020-05-03 12:36:53 +0200

    Klasse uitlening

--- Program/src/entity/Reservatie.java
+++ Program/src/entity/Reservatie.java
@@ -1,18 +1,24 @@
 package entity;
 
-import java.time.LocalDate;
+import java.util.Date;
+import java.util.*;
 
 public class Reservatie extends Transactie{
     private int reservatieId;
-    private LocalDate reservatieDatum;
+    private String reservatieDatum;
 
-    public int getReservatieId() { return reservatieId; }
+    public int getReservatieId() {
+        return reservatieId;
+    }
     public void setReservatieId(int reservatieId){
         this.reservatieId = reservatieId;
     }
 
-    public LocalDate getReservatieDatum() {
+    public String getReservatieDatum() {
         return reservatieDatum;
+    }
+    public void setReservatieDatum(String reservatieDatum){
+        this.reservatieDatum = reservatieDatum;
     }
 
     @Override
@@ -20,5 +26,20 @@ public class Reservatie extends Transactie{
         return super.toString() +
                 ", reservatieId = " + reservatieId
                 +", reservatiedatum = " + reservatieDatum;
+    }
+}
+public class HashSetBoek {
+    public static void main(String[] args) {
+        HashSet<Boek> set=new HashSet<>();
+        //Boeken aanmaken
+        Boek b1=new Boek(101, 9789401611039,"Zon","Lucinda Riley","Xander Uitgevers B.V.", "Nederlands", 732, 13-2-2020, 24.99, "RIRO",0, 2);
+        Boek b2=new Boek(102,9789026343339, "In hechtenis", "Nicci French", "Ambo|Anthos", "Nederlands", 437, 7-4-2020, 22.99, "FRRO", 1, 5);
+        //Boeken aan HashSet toevoegen
+        set.add(b1);
+        set.add(b2);
+        //HashSet overnemen
+        for(Boek b:set){
+            System.out.println(b.getArtikelnummer()+" "+b.getISBN()+" "+b.getTitel()+" "+b.getAuteur()+" "+b.getUitgeverij()+""+b.getTaal()+" "+b.getPaginas()+" "+b.getAankoopdatum()+" "+b.getPrijs()+" "+b.getPlaatsInBib()+""+b.isGereserveerd()+" "+b.getAantalKeerUitgeleend());
+        }
     }
 }
