adbe96e1d25ab857b109f86c50e1d5ffc4b00308
ckaers <charlienkaers@gmail.com>
2020-05-09 12:51:24 +0200

    update BoekDAO

--- Program/src/db/BoekDAO.java
+++ Program/src/db/BoekDAO.java
@@ -99,20 +99,21 @@ public class BoekDAO extends BaseDAO {
         }
     }
 
- //       public static void main(String[] args)  {
+        public static void main(String[] args)  {
         // Boek b = new Boek(9789029586665L, "De alchemist", "Paolo Coelho", "de Arbeiderspers", "Nederlands", 144, LocalDate.of(2000, Month.MAY, 15), 25.45, "COE135.2");
- //       BoekDAO bda = new BoekDAO();
+        BoekDAO bda = new BoekDAO();
  //       Boek b1 = new Boek (9789029586665L, "De alchemist", "Paolo Coelho", "de Arbeiderspers", "Nederlands", 144, LocalDate.of(2000, Month.MAY, 15), 25.45, "COE135.2"));
  //       bda.toevoegenBoek(b1);
  //       bda.ophalenBoeken();
 
-//        for(Boek b: bda.ophalenBoeken())
-//        {
-//            System.out.println(b.toString());
-//        }
+        for(Boek b: bda.ophalenBoeken())
+        {
+            System.out.println(b.toString());
+        }
 //        bda.toevoegenBoek(b1);
         // "-L" aan ISBN toevoegen -  aanzien als een long in plaats van een int
+
 //            if(bda.opzoekenBoek("De Bourgondiërs", ))
-//    }
+    }
 
 }
