df9b1f39b68e310b9636bc3765eec9d179e9a73d
Bart <bart.tassignon@student.ehb.be>
2020-05-09 16:47:16 +0200

    OpzoekenLezerForm werkend weergegeven in Jpanel

--- Program/src/GUI/OpzoekenLezerForm.java
+++ Program/src/GUI/OpzoekenLezerForm.java
@@ -4,9 +4,6 @@
 
 package GUI;
 
-import db.LezerDAO;
-import entity.Lezer;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -16,9 +13,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 public class OpzoekenLezerForm extends JFrame {
 
@@ -29,8 +24,6 @@ public class OpzoekenLezerForm extends JFrame {
     private JTextField naamTextField;
     private JFrame opzoekenLezerFrame = new JFrame();
     private JPanel panel1;
-
-
     private JButton TerugButton;
 
     DefaultTableModel model = new DefaultTableModel();
@@ -42,7 +35,9 @@ public class OpzoekenLezerForm extends JFrame {
         new OpzoekenLezerForm();
     }
 
-    public void OpzoekenLezers() {
+    public OpzoekenLezerForm() {
+
+
         cnt.setLayout(new FlowLayout(FlowLayout.LEFT));
         model.addColumn("LezerId");
         model.addColumn("Voornaam");
@@ -63,45 +58,18 @@ public class OpzoekenLezerForm extends JFrame {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
         JScrollPane pg = new JScrollPane(jtbl);
         cnt.add(pg);
         this.pack();
-    }
 
-    public OpzoekenLezerForm() {
-
-
-        opzoekenLezerFrame.getContentPane().add(panel1);
+        //opzoekenLezerFrame.getContentPane().add(panel1);
+        opzoekenLezerFrame.getContentPane().add(jtbl);
         opzoekenLezerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         opzoekenLezerFrame.setVisible(true);
         opzoekenLezerFrame.setSize(600, 600);
         opzoekenLezerFrame.setResizable(false);
         opzoekenLezerFrame.setLocationRelativeTo(null);
-
-        cnt.setLayout(new FlowLayout(FlowLayout.LEFT));
-        model.addColumn("LezerId");
-        model.addColumn("Voornaam");
-        model.addColumn("Naam");
-        model.addColumn("Geboortedatum");
-        model.addColumn("Emailadres");
-        model.addColumn("Telefoon");
-        try {
-            String url = "jdbc:mysql://dt5.ehb.be/1920mobappgr1";
-            String user = "1920mobappgr1";
-            String password = "XNnhDjw";
-            Connection con = DriverManager.getConnection(url, user, password);
-            PreparedStatement pstm = con.prepareStatement("select * from Lezers");
-            ResultSet Rs = pstm.executeQuery();
-            while (Rs.next()) {
-                model.addRow(new Object[]{Rs.getInt(1), Rs.getString(2), Rs.getString(3), Rs.getObject(4, LocalDate.class), Rs.getString(5), Rs.getString(6)});
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        JScrollPane pg = new JScrollPane(jtbl);
-        cnt.add(pg);
-        this.pack();
-
 
 
         TerugButton.addActionListener(new ActionListener() {
@@ -112,6 +80,8 @@ public class OpzoekenLezerForm extends JFrame {
             }
         });
     }
+
+
 }
 
 
