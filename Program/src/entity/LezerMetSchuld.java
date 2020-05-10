0238b78d75dbc8e59302bc272c684cbb62452a27
katrienPersoons <katrien.persoons@student.ehb.be>
2020-05-10 08:59:40 +0200

    Toevoegen serialversionUID bij exceptions

---
+++ Program/src/entity/LezerMetSchuld.java
@@ -0,0 +1,8 @@
+package entity;
+
+public class LezerMetSchuld extends Exception {
+    private static final long serialVersionUID = -4290356255934152012L;
+
+    public LezerMetSchuld() {
+    }
+}
--- Program/src/entity/LezerTeJong.java
+++ Program/src/entity/LezerTeJong.java
@@ -1,6 +1,9 @@
 package entity;
 
 public class LezerTeJong extends Exception {
+
+    private static final long serialVersionUID = -9011271642664265852L;
+
     public LezerTeJong() {
     }
 }
