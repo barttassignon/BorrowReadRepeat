e3062b133e8896b1392fcae93a4390219818d179
Bart <bart.tassignon@student.ehb.be>
2020-05-13 17:29:07 +0200

    OnzeLogger warning toegevoegd. Zijn de toplevels die er nu inzitten (Info, Warning, Severe)

--- Program/src/entity/OnzeLogger.java
+++ Program/src/entity/OnzeLogger.java
@@ -6,41 +6,47 @@
 package entity;
 
 import java.io.IOException;
-import java.util.logging.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class OnzeLogger {
     Logger OnzeLogger = Logger.getLogger(OnzeLogger.class.getName());
     FileHandler fh = null;
 
-public OnzeLogger() {
+    public OnzeLogger() {
 
-    try {
-        //schrijf log weg naar logfile
-        fh = new FileHandler("OnzeLog.log", true);
+        try {
+            //schrijf log weg naar logfile
+            fh = new FileHandler("OnzeLog.log", true);
 
-    } catch (SecurityException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //formatter voor de teks in logfile
+        fh.setFormatter(new SimpleFormatter());
+        OnzeLogger.addHandler(fh);
     }
 
-    //formatter voor de teks in logfile
-    fh.setFormatter(new SimpleFormatter());
-    OnzeLogger.addHandler(fh);
+    //logger voor info log
+    public void doLoggingInfo(String tekst) {
+        OnzeLogger.info(tekst);
+        // geen bericht in console voor info berichten
+        OnzeLogger.setUseParentHandlers(false);
+    }
 
-}
+    //logger voor warning log
+    public void doLoggingWarning(String tekst) {
+        OnzeLogger.warning(tekst);
+    }
 
-//logger voor info log
-public void doLoggingInfo(String tekst){
-    OnzeLogger.info(tekst);
-
-}
-
-//logger voor severe log
-public void doLoggingSevere(String tekst) {
-    OnzeLogger.severe(tekst);
-}
-
+    //logger voor severe log
+    public void doLoggingSevere(String tekst) {
+        OnzeLogger.severe(tekst);
+    }
 }
 
 
